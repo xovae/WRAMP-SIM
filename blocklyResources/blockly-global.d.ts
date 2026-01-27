33bd0dcba0521dfcb68dfafe6d1c06ca8356c6e8
xovae <89520672+xovae@users.noreply.github.com>
2026-01-27 13:32:58 +1300

    Test upload of website

---
+++ blocklyResources/blockly-global.d.ts
@@ -0,0 +1,3 @@
+//Adds VSCode autofill for Blockly methods
+declare const Blockly: typeof import("./Node/node_modules/blockly/core");
+declare const wrampGenerator: typeof import("./wrampGenerator.js")
