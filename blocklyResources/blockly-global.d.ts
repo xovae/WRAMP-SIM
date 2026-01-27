f3dda82f977ef9ffbd1412a36a055a0ee8c272cb
Jamie Boulton <jb469@students.waikato.ac.nz>
2026-01-28 10:05:01 +1300

    Removing all resources

--- blocklyResources/blockly-global.d.ts
+++
@@ -1,3 +0,0 @@
-//Adds VSCode autofill for Blockly methods
-declare const Blockly: typeof import("./Node/node_modules/blockly/core");
-declare const wrampGenerator: typeof import("./wrampGenerator.js")
